277dc9a4d519f7e09bb177b78795bd6002b5bf65
Dogpa <dogpa0130@gmail.com>
2022-07-06 08:13:51 +0800

    Initial Commit

---
+++ Swift Practice 164 Slider Toggle Filter/Swift_Practice_164_Slider_Toggle_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_164_Slider_Toggle_FilterApp.swift
+//  Swift Practice 164 Slider Toggle Filter
+//
+//  Created by Dogpa's MBAir M1 on 2022/7/6.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_164_Slider_Toggle_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
